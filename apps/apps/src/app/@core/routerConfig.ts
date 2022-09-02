8d06668ad57fcc8e19f9daa9695c6cc7b54b37bd
NsdHSO (Nechiforel David-Samuel) <nechiforelsamuel@yahoo.com>
2022-09-02 10:00:26 +0300

    feat: added icon module frame

---
+++ apps/apps/src/app/@core/routerConfig.ts
@@ -0,0 +1,9 @@
+export const CONFIG_MAIN = Object.freeze({
+    routerDataConfig: [{
+      path: '',
+      icon: 'fa_solid:arrow-down',
+      text: 'Driver'
+    }
+    ],
+    iconApp: ''
+  });
--- apps/apps/src/app/app.module.ts
+++ apps/apps/src/app/app.module.ts
@@ -1,13 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }         from '@angular/core';
+import { MatIconModule }    from "@angular/material/icon";
+import { BrowserModule }    from '@angular/platform-browser';
+import { FrameWholeModule } from 'ngx-liburg-frame-side';
+import { IconCoreModule }   from "ngx-liburg-icon";
+import { CONFIG_MAIN }      from './@core/routerConfig';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
-  bootstrap: [AppComponent],
-})
+@NgModule ( {
+  declarations : [ AppComponent ],
+  imports : [
+    BrowserModule,
+    AppRoutingModule,
+    FrameWholeModule.forRoot ( CONFIG_MAIN ),
+    IconCoreModule,
+    MatIconModule,
+
+  ],
+  providers    : [],
+  bootstrap    : [ AppComponent ],
+} )
 export class AppModule {}
