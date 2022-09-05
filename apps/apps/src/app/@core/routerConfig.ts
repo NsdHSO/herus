c1dca7c1afed0652814001e61cd68c50689b524d
NsdHSO (Nechiforel David-Samuel) <nechiforelsamuel@yahoo.com>
2022-09-06 01:18:40 +0300

    refactor: added driver path

--- apps/apps/src/app/@core/routerConfig.ts
+++ apps/apps/src/app/@core/routerConfig.ts
@@ -1,13 +1,13 @@
 export const CONFIG_MAIN = Object.freeze({
   routerDataConfig: [
     {
-      path: '',
+      path: 'driver',
       icon: 'fa_solid:arrow-down',
       text: 'Driver',
     },
     {
       path: 'truck',
-      icon: 'fa_solid:arrow-down',
+      icon: 'fa_solid:truck',
       text: 'Truck',
     },
   ],
